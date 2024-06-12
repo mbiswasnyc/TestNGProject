3572a2b0802c4c2fe3ca2b1464ae8914dc3a5ac9
mbiswas <mbiswasnyc@gmail.com>
2024-06-12 01:50:59 -0400

    TestNG 1st Push to main

---
+++ src/test/java/TestNG1/TestClass1.java
@@ -0,0 +1,20 @@
+package TestNG1;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+    @Test
+    public void test1(){
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Test3");
+    }
+}
